4e3888b60f133785bc5f3fb1e06e26c630da1ed8
Cibin <134275527+CibinBK@users.noreply.github.com>
2023-05-25 11:55:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 // Importing
 const express = require('express');
-const { model } = require('mongoose');
-const studentSchema = require('./model/studentdb')
-//Initialize
 const app = new express();
+const { model } = require('mongoose');
+const studentModel = require('./model/studentdb')
+//Initialize
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 //Api create
+
+app.get('/view',(req,res)=>{
+    res.json({"name":"Cibin","age":12});
+})
+//Port
+app.listen(3030,(req,res)=>{
+    console.log("App is running in port:8080")
+})
+
 app.post('/create',(req,res)=>{
     var result = new studentModel(req.body);
     result.save();
     res.send("data added");
 })
-app.get('/view',(req,res)=>{
-    res.json({"name":"Cibin","age":12});
-})
-//Port
-app.listen(8080,()=>{
-    console.log("App is running in port:8080")
+
+app.get('/see',(req,res)=>{
+    var data = studentModel.find();
+    res.json(data);
 })
